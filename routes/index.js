5f532495c80f9c49c33fb91b49efbfab54a082ac
Morthrall <the.asteroth@gmail.com>
2012-09-08 11:16:58 +0200

    bootstrap init
    there is some problem with jade source code...

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	var connectionString = process.env.DATABASE_URL || 'postgres://root:root@localhost:5432/sound_motion';
+	var pg = require('pg');
+	
+	pg.connect(connectionString, function(error, client) {
+		if (error) {
+			console.log(error);
+			res.render('index', {title: 'Express', error: error});
+		} else {
+			client.query('SELECT * FROM movie', function(error, result) {
+				if (error) {
+					console.log(error);
+					res.render('index', {title: 'Express', error: error});
+				} else {
+					res.render('index', {title: 'Express', result: result});
+				}
+			});
+		}
+	});
 };
