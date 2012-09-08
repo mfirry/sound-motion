4e8fe070b8ebde0602727d98c3136083e72f63f8
Morthrall <the.asteroth@gmail.com>
2012-09-08 11:43:06 +0200

    fixed problems with jade, problems with postgres

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 
 exports.index = function(req, res){
 	var connectionString = process.env.DATABASE_URL || 'postgres://root:root@localhost:5432/sound_motion';
-	var pg = require('pg');
+	var pg = require('pg').native;
 	
 	pg.connect(connectionString, function(error, client) {
 		if (error) {
 			console.log(error);
-			res.render('index', {title: 'Express', error: error});
+			res.render('index', {title: 'Express', error: error, result: false});
 		} else {
 			client.query('SELECT * FROM movie', function(error, result) {
 				if (error) {
 					console.log(error);
-					res.render('index', {title: 'Express', error: error});
+					res.render('index', {title: 'Express', error: error, result: false});
 				} else {
-					res.render('index', {title: 'Express', result: result});
+					res.render('index', {title: 'Express', error: false, result: result});
 				}
 			});
 		}
