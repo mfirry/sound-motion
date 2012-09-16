9ea25cb4a256acec56849994454f813cf5f45270
Morthrall <the.asteroth@gmail.com>
2012-09-16 16:08:34 +0200

    better queries

--- routes/index.js
+++ routes/index.js
@@ -21,9 +21,9 @@ exports.index = function(req, res){
 	var nextSunday = sunday.clone()
 	nextSunday.addDays(7);
 	var weeks = [
-		{monday: lastMonday.toFormat(dateFormat), sunday: lastSunday.toFormat(dateFormat)},
-		{monday: monday.toFormat(dateFormat), sunday: sunday.toFormat(dateFormat)},
-		{monday: nextMonday.toFormat(dateFormat), sunday: nextSunday.toFormat(dateFormat)}
+		{monday: lastMonday, sunday: lastSunday},
+		{monday: monday, sunday: sunday},
+		{monday: nextMonday, sunday: nextSunday}
 	];
 
 	var db = new dbi.DB(USE_DBTYPE == dbi.DBTYPE_POSTGRES ? {
@@ -42,7 +42,43 @@ exports.index = function(req, res){
 		password: 'root'
 	});
 	
-	var mainQuery = 'SELECT distinct movie_id AS id FROM screening where date >=\'' + lastMonday.toYMD('-') + '\' AND date <=\'' + nextSunday.toYMD('-') + '\'';
+	var callbacks = [];
+	for (var i = 0; i < weeks.length; i++) {
+		var week = weeks[i];
+		var mainQuery = 'SELECT movie.id AS id, title, link, note, `date`, `time` FROM screening JOIN movie ON screening.movie_id = movie.id WHERE `date` >= \'' + week.monday.toYMD() + '\' AND `date` <= \'' + week.sunday.toYMD() + '\'';
+		(function(query) {
+			callbacks.push(function(callback) {
+				db.query(query, function(results) {
+					var movies = {};
+					for (var j = 0; j < results.length; j++) {
+						var movie = results[j];
+						if (movies[movie.id]) {
+							movies[movie.id].dates.push({date: movie.date, time: movie.time});
+						} else {
+							movies[movie.id] = {
+								id: movie.id,
+								title: movie.title,
+								note: movie.note,
+								dates: [{date: movie.date, time: movie.time}]
+							};
+						}
+					}
+					callback(null, movies);
+				});
+			});
+		})(mainQuery);
+	}
+	async.parallel(callbacks, function(error, results){
+		if (error) {
+			console.log(error);
+			res.render('index', {error: error, weeks: false});
+		} else {
+			console.log(results);
+			res.render('index', {error: error, weeks: results});
+		}
+	});
+	
+	/*var mainQuery = 'SELECT distinct movie_id AS id FROM screening where date >=\'' + lastMonday.toYMD('-') + '\' AND date <=\'' + nextSunday.toYMD('-') + '\'';
 	db.query(mainQuery, function(results) {
 		var callbacks = [];
 		for (var i = 0; i < results.length; i++) {
@@ -67,5 +103,5 @@ exports.index = function(req, res){
 	}, function(error) {
 		console.log(error);
 		res.render('index', {title: 'Express', error: error, result: false, weeks: weeks});
-	});
+	});*/
 };
