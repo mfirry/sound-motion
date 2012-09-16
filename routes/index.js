7a925f930df07e7d0b30c9d1100b2ac1fde84f80
Morthrall <the.asteroth@gmail.com>
2012-09-16 13:23:25 +0200

    parallel queries

--- routes/index.js
+++ routes/index.js
@@ -1,75 +1,8 @@
 require('date-utils');
+var async = require('async');
+var dbi = require('../libs/db');
 
-var DBTYPE_POSTGRES = 'pg';
-var DBTYPE_MYSQL = 'mysql';
-
-var USE_DBTYPE = DBTYPE_POSTGRES;
-
-var DB = function(config) {
-	this.config = config || {};
-	var defaultConfig = {
-		engineType: DBTYPE_POSTGRES,
-		host: 'localhost',
-		port: '5432',
-		database: 'sound_motion',
-		user: 'root',
-		password: 'root'
-	};
-	for (var field in defaultConfig)
-		if (!this.config[field])
-			this.config[field] = defaultConfig[field];
-
-	this.engine = require(this.config.engineType);
-	
-	if (this.config.engineType == DBTYPE_POSTGRES) {
-		this.client = this.engine;
-		this.connectionString = 'postgres://' + this.config.user + ':' + this.config.password + '@' + this.config.host + ':' + this.config.port + '/' + this.config.database;
-		this.query = function(query, callback, errorCallback) {
-			this.engine.connect(this.connectionString, function(error, client) {
-				if (error) {
-					if (errorCallback) {
-						errorCallback(error);
-					} else {
-						console.log(error);
-					}
-				} else {
-					client.query(query, function(error, result) {
-						if (error) {
-							if (errorCallback) {
-								errorCallback(error);
-							} else {
-								console.log(error);
-							}
-						} else {
-							callback(result.rows);
-						}
-					});
-				}
-			});
-		};
-	} else if (this.config.engineType == DBTYPE_MYSQL) {
-		this.client = this.engine.createClient({
-			host: this.config.host,
-			port: this.config.port,
-			database: this.config.database,
-			user: this.config.user,
-			password: this.config.password
-		});
-		this.query = function(query, callback, errorCallback) {
-			this.client.query(query, [], function (error, result, fields) {
-				if (error) {
-					if (errorCallback) {
-						errorCallback(error);
-					} else {
-						console.log(error);
-					}
-				} else {
-					callback(result);
-				}
-			});
-		};
-	}
-}
+var USE_DBTYPE = dbi.DBTYPE_MYSQL;
 
 exports.index = function(req, res){
 	var dateFormat = 'YYYY-MM-DD';
@@ -93,7 +26,7 @@ exports.index = function(req, res){
 		{monday: nextMonday.toFormat(dateFormat), sunday: nextSunday.toFormat(dateFormat)}
 	];
 
-	var db = new DB(USE_DBTYPE == DBTYPE_POSTGRES ? {
+	var db = new dbi.DB(USE_DBTYPE == dbi.DBTYPE_POSTGRES ? {
 		engineType: DBTYPE_POSTGRES,
 		host: 'localhost',
 		port: '5432',
@@ -101,7 +34,7 @@ exports.index = function(req, res){
 		user: 'root',
 		password: 'root'
 	} : {
-		engineType: DBTYPE_MYSQL,
+		engineType: dbi.DBTYPE_MYSQL,
 		host: 'localhost',
 		port: '3306',
 		database: 'sound_motion',
@@ -109,18 +42,26 @@ exports.index = function(req, res){
 		password: 'root'
 	});
 	
-	var movies = new Array();
-	var query1 = 'SELECT distinct movie_id AS id FROM screening where date >=\''+lastMonday.toYMD('-')+'\' UNION SELECT distinct movie_id FROM screening where date <=\''+nextSunday.toYMD('-')+'\'';
-	db.query(query1, function(resultZ) {
-		console.log(resultZ);
-		for (i=0; i<resultZ.length; i++) {
-			//console.log(result[i]);
-			db.query('SELECT * FROM movie where id='+resultZ[i].id, function(result2) {
-					movies.push(result2[0]);
-				}
-			);
+	var mainQuery = 'SELECT distinct movie_id AS id FROM screening where date >=\'' + lastMonday.toYMD('-') + '\' UNION SELECT distinct movie_id FROM screening where date <=\'' + nextSunday.toYMD('-') + '\'';
+	db.query(mainQuery, function(results) {
+		var callbacks = [];
+		for (var i = 0; i < results.length; i++) {
+			var secondQuery = 'SELECT * FROM movie where id = ' + results[i].id;
+			callbacks.push(function(callback) {
+				console.log(secondQuery);
+				db.query(secondQuery, function(result) {
+					callback(null, result);
+				});
+			});
 		}
-		res.render('index', {title: 'Express', error: false, result: movies, weeks: weeks});
+		async.parallel(callbacks, function(error, results){
+			if (error) {
+				console.log(error);
+				res.render('index', {title: 'Express', error: error, result: false, weeks: weeks});
+			} else {
+				res.render('index', {title: 'Express', error: error, result: results, weeks: weeks});
+			}
+		});
 	}, function(error) {
 		console.log(error);
 		res.render('index', {title: 'Express', error: error, result: false, weeks: weeks});
