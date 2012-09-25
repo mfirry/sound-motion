0c6129610aaa4b42806463632d0f63b7f1a6a2a0
Marco F <mfirry@gmail.com>
2012-09-25 22:48:28 +0200

    124556

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-var port = process.env.PORT || 3000;
+var port = app.get('port') || 3000;
 app.listen(port, function(){
 	console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
