913aa16c6b4f4aa82e0757962d74479fd8f8b356
Marco Firrincieli <mfirry@gmail.com>
2012-10-25 15:43:01 +0200

    more cleaning

--- data_entry.js
+++ data_entry.js
@@ -2,7 +2,7 @@ exports.movies = new Array({
     "title": "THE DARK KNIGHT RISES",
     "description": "The epic conclusion to filmmaker Christopher Nolan's Batman trilogy. After eight years of struggling with his new status as a wanted vigilante, Batman must contend with newcomers Catwoman and the brutal and villainous Bane.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-10-08T15:00")), new Date(Date.parse("2012-10-08T18:10")), new Date(Date.parse("2012-10-08T21:15"))]
     }, {
         "venue": "Arcobaleno",
@@ -15,7 +15,7 @@ exports.movies = new Array({
     "title": "WHAT TO EXPECT WHEN YOU’RE EXPECTING",
     "description": "Based on the 16 million copy best-selling book, What to Expect When You're Expecting is an ensemble romantic-comedy is in the vein of Love Actually and Valentine's Day. The film is a modern look at love through the eyes of four interconnected couples experiencing the thrills and surprises of having a baby.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-10-15T13:00")), new Date(Date.parse("2012-10-15T15:20")), new Date(Date.parse("2012-10-15T17:40")), new Date(Date.parse("2012-10-15T20:00")), new Date(Date.parse("2012-10-15T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -28,7 +28,7 @@ exports.movies = new Array({
     "title": "PROMETHEUS",
     "description": "In the distant future, two superpowers control Earth and fight each other for all the solar system's natural resources. When one side dispatches a team to a distant planet to terraform it for human colonization, the team discovers an indigenous race of bio-mechanoid killers.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-10-22T13:00")), new Date(Date.parse("2012-10-22T15:20")), new Date(Date.parse("2012-10-22T17:40")), new Date(Date.parse("2012-10-22T20:00")), new Date(Date.parse("2012-10-22T22:15"))]
     }, {
         "venue": "Arcobaleno",
@@ -41,7 +41,7 @@ exports.movies = new Array({
     "title": "THE WORDS",
     "description": "In The Words, an aspiring writer claims another man’s long lost work as his own only to find there are steep consequences when ambition is valued above life’s most fundamental, impassioned three words.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-10-29T13:00")), new Date(Date.parse("2012-10-29T15:20")), new Date(Date.parse("2012-10-29T17:40")), new Date(Date.parse("2012-10-29T20:00")), new Date(Date.parse("2012-10-22T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -51,7 +51,7 @@ exports.movies = new Array({
     "title": "MAGIC MIKE",
     "description": "Set in the world of male strippers, Magic Mike is directed by Steven Soderbergh and stars Channing Tatum (Dear John, Step Up) in a story inspired by his real life. The film follows Mike (Tatum) as he takes a young dancer called The Kid (Pettyfer) under his wing and schools him in the fine arts of partying, picking up women, and making easy money.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-11-05T13:00")), new Date(Date.parse("2012-11-05T15:20")), new Date(Date.parse("2012-11-05T17:40")), new Date(Date.parse("2012-11-05T20:00")), new Date(Date.parse("2012-11-05T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -64,7 +64,7 @@ exports.movies = new Array({
     "title": "ON THE ROAD",
     "description": "Adaptation of Jack Kerouac's On the Road, which chronicles Kerouac's years traveling the North American continent with his friend Neal Cassady, a sideburned hero of the snowy West. As Sal Paradise and Dean Moriarty, the two roam the country in a quest for self-knowledge and experience.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-11-12T13:00")), new Date(Date.parse("2012-11-12T15:40")), new Date(Date.parse("2012-11-12T18:30")), new Date(Date.parse("2012-11-12T21:15"))]
     }, {
         "venue": "Arcobaleno",
@@ -77,7 +77,7 @@ exports.movies = new Array({
     "title": "SAVAGES",
     "description": "Laguna Beach entrepreneurs Ben (Johnson), a peaceful and charitable marijuana producer, and his closest friend Chon (Kitsch), a former Navy SEAL and ex-mercenary, run a lucrative, homegrown industry-raising some of the best weed ever developed.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-11-19T12:30")), new Date(Date.parse("2012-11-19T15:00")), new Date(Date.parse("2012-11-19T17:30")), new Date(Date.parse("2012-11-19T20:00")), new Date(Date.parse("2012-11-19T22:30"))]
     }, {
         "venue": "Arcobaleno",
@@ -90,7 +90,7 @@ exports.movies = new Array({
     "title": "KILLING THEM SOFTLY",
     "description": "Adapted from George V. Higgins novel and set in New Orleans, Killing Them Softly follows professional enforcer, Jackie Cogan (Pitt), who investigates a heist that occurs during a high stakes, mob-protected, poker game.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-11-26T13:00")), new Date(Date.parse("2012-11-26T15:20")), new Date(Date.parse("2012-11-26T17:40")), new Date(Date.parse("2012-11-26T20:00")), new Date(Date.parse("2012-11-26T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -103,7 +103,7 @@ exports.movies = new Array({
     "title": "ARGO",
     "description": "Based on true events, Argo chronicles the life-or-death covert operation to rescue six Americans, which unfolded behind the scenes of the Iran hostage crisis--the truth of which was unknown by the public for decades. On November 4, 1979, as the Iranian revolution reaches its boiling point, militants storm the U.S. embassy in Tehran, taking 52 Americans hostage.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-12-03T13:00")), new Date(Date.parse("2012-12-03T15:20")), new Date(Date.parse("2012-12-03T17:40")), new Date(Date.parse("2012-12-03T20:00")), new Date(Date.parse("2012-12-03T22:15"))]
     }, {
         "venue": "Arcobaleno",
@@ -116,7 +116,7 @@ exports.movies = new Array({
     "title": "HOPE SPRINGS",
     "description": "Kay and Arnold are a devoted couple, but decades of marriage have left Kay wanting to spice things up and reconnect with her husband. When she hears of a renowned couple's specialist in the small town of Great Hope Springs, she attempts to persuade her skeptical husband, a steadfast man of routine, to get on a plane for a week of marriage therapy.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2012-12-10T13:00")), new Date(Date.parse("2012-12-10T15:20")), new Date(Date.parse("2012-12-10T17:40")), new Date(Date.parse("2012-12-10T20:00")), new Date(Date.parse("2012-12-10T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -129,7 +129,7 @@ exports.movies = new Array({
     "title": "TROUBLE WITH THE CURVE",
     "description": "Gus Lobel (Clint Eastwood) has been one of the best scouts in baseball for decades, but, despite his efforts to hide it, age is starting to catch up with him. Nevertheless, Gus—who can tell a pitch just by the crack of the bat—refuses to be benched for what may be the final innings of his career. He may not have a choice.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2013-01-07T13:00")), new Date(Date.parse("2013-01-07T15:20")), new Date(Date.parse("2013-01-07T17:40")), new Date(Date.parse("2013-01-07T20:00")), new Date(Date.parse("2013-01-07T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -142,7 +142,7 @@ exports.movies = new Array({
     "title": "MOONRISE KINGDOM",
     "description": "Set on an island off the coast of New England in the 1960s, Moonrise Kingdom follows a young boy and girl falling in love. When they are moved to run away together, various factions of the town mobilize to search for them and the town is turned upside down – which might not be such a bad thing.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2013-01-14T13:00")), new Date(Date.parse("2013-01-14T15:20")), new Date(Date.parse("2013-01-14T17:40")), new Date(Date.parse("2013-01-14T20:00")), new Date(Date.parse("2013-01-14T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -155,7 +155,7 @@ exports.movies = new Array({
     "title": "THE HOBBIT: AN UNEXPECTED JOURNEY",
     "description": "The Hobbit: An Unexpected Journey follows title character Bilbo Baggins, who is swept into an epic quest to reclaim the lost Dwarf Kingdom of Erebor, which was long ago conquered by the dragon Smaug. Approached out of the blue by the wizard Gandalf the Grey, Bilbo finds himself joining a company of thirteen dwarves led by the legendary warrior Thorin Oakenshield.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2013-01-21T15:00")), new Date(Date.parse("2013-01-21T18:10")), new Date(Date.parse("2013-01-21T21:15"))]
     }, {
         "venue": "Arcobaleno",
@@ -168,7 +168,7 @@ exports.movies = new Array({
     "title": "LOVE IS ALL YOU NEED",
     "description": "Love is all you need is a romantic comedy by Oscar winner Susanne Bier written by Susanne Bier and Anders Thomas Jensen, starring Pierce Brosnan and Trine Dyrholm. The film is a warm, funny and life-affirming story about how, just when you think its all over, it just might be about to begin.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2013-01-28T13:00")), new Date(Date.parse("2013-01-28T15:20")), new Date(Date.parse("2013-01-28T17:40")), new Date(Date.parse("2013-01-28T20:00")), new Date(Date.parse("2013-01-28T22:00"))]
     }, {
         "venue": "Arcobaleno",
@@ -181,7 +181,7 @@ exports.movies = new Array({
     "title": "THE ANGEL'S SHARE",
     "description": "Narrowly avoiding jail, new dad Robbie vows to turn over a new leaf. A visit to a whisky distillery inspires him and his mates to seek a way out of their hopeless lives.",
     "screenings": [{
-        "venue": "anteo",
+        "venue": "Anteo",
         "dates": [new Date(Date.parse("2013-02-04T13:00")), new Date(Date.parse("2013-02-04T15:20")), new Date(Date.parse("2013-02-04T17:40")), new Date(Date.parse("2013-02-04T20:00")), new Date(Date.parse("2013-02-04T22:00"))]
     }, {
         "venue": "Arcobaleno",
