5131d4aa26ecdb44325a1f29f287f2d7c3294694
Francesco Negri <francesconegri@gmail.com>
2012-10-28 11:33:36 -0400

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 /**
  * Module dependencies.
  */
-
-var moment = require('moment');
-
 var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , cons = require('consolidate')
-  , name = 'mustache';
+  , path    = require('path')
+  , cons    = require('consolidate')
+  , moment  = require('moment');
   
 var app = express();
 
@@ -16,7 +12,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
-    app.engine('.html', cons.mustache);
+  app.engine('.html', cons.mustache);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -31,14 +27,17 @@ app.configure('development', function(){
 
 // DB connection: connect here, use it everywhere
 var mongoose = require('mongoose');
-try {
-  db = mongoose.connect(process.env.MONGOHQ_URL);
-} catch(e) {
-  db = mongoose.createConnection('localhost', 'local');
+
+if (process.argv[2]) {
+  var db = mongoose.connect(process.argv[2]);
+} else if (process.env.MONGOHQ_URL) {
+  var db = mongoose.connect(process.env.MONGOHQ_URL);
+} else {
+  var db = mongoose.connect('mongodb://localhost/local');
 }
 
 app.get('/test', function(req, res){
-    res.render('test');
+  res.render('index');
 });
 
 app.get('/', function(req, res){
@@ -49,51 +48,45 @@ app.get('/', function(req, res){
   var Movie = db.model('Movie',movie);
   var Screening = db.model('Screening', screening)
 
-    // console.log(moment(new Date()));
-    // console.log(moment(new Date()).day(0));
-    // console.log(moment(new Date()).day(14));
+  // console.log(moment(new Date()));
+  // console.log(moment(new Date()).day(0));
+  // console.log(moment(new Date()).day(14));
 
   var lastSunday = moment(new Date()).day(-7).toDate();
   var nextSunday = moment(new Date()).day(14).toDate();
 
-    Movie.find({"screenings.dates":{$gte:lastSunday, $lte:nextSunday}}, function(err, movies){
-        console.log(movies[0].screenings[0].dates[0]);
-        // try {
-        //   // heroku apparently needs to disconnect after each query
-        //   db.disconnect();
-        // } catch (e) {
-        //   // ...but Object #<NativeConnection> has no method 'disconnect'
-        // }
+  Movie.find({"screenings.dates":{$gte:lastSunday, $lte:nextSunday}}, function(err, movies){
+    console.log(movies[0].screenings[0].dates[0]);
 
-        // FIXME: this is way too lame
-        movies[0].class = "label";
-        movies[0].week  = "Last week";
-        movies[1].class = "label label-success";
-        movies[1].week  = "This week";
-        movies[2].class = "label label-info";
-        movies[2].week  = "Next week";
+    // FIXME: this is way too lame
+    movies[0].class = "label";
+    movies[0].week  = "Last week";
+    movies[1].class = "label label-success";
+    movies[1].week  = "This week";
+    movies[2].class = "label label-info";
+    movies[2].week  = "Next week";
 
-        res.render('index', {
-            movies: movies,
-            the_date: function() {
-                return function(text, render) {
-                    var date = moment(new Date(render(text)));
-                    return date.format("MMMM, D YYYY").replace(/ /g,'&nbsp;');
-                }
-            },
-            the_time: function() {
-                return function(text, render) {
-                    var date = moment(new Date(render(text)));
-                    return date.format("HH:mm");
-                }
-            }            
-        });
+    res.render('index', {
+      movies: movies,
+      the_date: function() {
+        return function(text, render) {
+          var date = moment(new Date(render(text)));
+          return date.format("MMMM, D YYYY").replace(/ /g,'&nbsp;');
+        }
+      },
+      the_time: function() {
+        return function(text, render) {
+          var date = moment(new Date(render(text)));
+          return date.format("HH:mm");
+        }
+      }
     });
+  });
     
-    // Movie.find({}, function(err, screenings){
-    //     console.log(screenings);
-    //     res.send(screenings);
-    // });
+  // Movie.find({}, function(err, screenings){
+  //     console.log(screenings);
+  //     res.send(screenings);
+  // });
 });
 
 app.get('/create', function(req, res) {
@@ -115,7 +108,7 @@ app.get('/create', function(req, res) {
     Movie.remove({}, function(){
       console.log("== REMOVED ALL MOVIES ==")
       // Fill it with the objects from the data_entry file
-      for (k in de.movies) {
+      for (var k in de.movies) {
         console.log('inserting :' + de.movies[k].title);
         new Movie(de.movies[k]).save();
       }
@@ -123,10 +116,6 @@ app.get('/create', function(req, res) {
     });
 });
 
-app.listen(app.get('port') || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+app.listen(app.get('port') || process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
-
-/*http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});*/
