ad787ffc6cd080fe239823d3da80c2cbe9574ce1
Marco Firrincieli <mfirry@gmail.com>
2012-12-11 20:25:53 +0100

    fix missing week

--- app.js
+++ app.js
@@ -64,8 +64,10 @@ app.get('/', function(req, res){
     // movies[0].week  = "Last week";
     movies[0].class = "label label-success";
     movies[0].week  = "This week";
-    movies[1].class = "label label-info";
-    movies[1].week  = "Next week";
+    if() {
+      movies[1].class = "label label-info";
+      movies[1].week  = "Next week";
+    }
 
     res.render('index', {
       movies: movies,
