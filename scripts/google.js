a041b6d1363d784a1161eb2da18f83e617c2358c
Francesco Negri <francesconegri@gmail.com>
2013-02-02 21:44:47 +0100

    google spreadsheet import
    the schema is not correct, though, as screenings of the same film
    in different venues are not yet grouped together.

---
+++ scripts/google.js
@@ -0,0 +1,41 @@
+var doc = "0AtFF6SiRVHtIdEpzelh6S21OODNLeHpTZ2FWWjU3WWc";
+
+var           _ = require("underscore");
+var Spreadsheet = require("spreadsheet");
+var database    = require('../database');
+
+var insert = function() {
+
+  var sheet = new Spreadsheet(doc);
+  sheet.worksheets(function(err,ws){
+    console.log("=== Sheet: " + ws.title);
+    var venue = ws.title;
+    ws.eachRow(function(err,row,meta){
+      var movie = new Object();
+      movie.title = row.title;
+      movie.imdb = row.imdb;
+      movie.description = row.description;
+      movie.screenings = [];
+      _.each([row.screening1, row.screening2, row.screening3, row.screening4,
+             row.screening5], function(s) {
+               // If typeof === object, the cell is probably empty
+               if (typeof(s)!=="object") movie.screenings.push(s);
+      });
+      database.Movie.create(movie, function(err){
+        if (err) console.log(err);
+        else console.log("Inserted: " + venue + " - " + movie.title);
+      });
+      // database.Movie.update({title: movie.title}, movie, {upsert: true}, function(err){
+      //   console.log("- " + movie.title);
+      //   if (err) console.log(err);
+      // });
+    })
+  });
+
+};
+
+database.Movie.remove({}, function(){
+  console.log("!! Removed all movies");
+  insert();
+});
+
